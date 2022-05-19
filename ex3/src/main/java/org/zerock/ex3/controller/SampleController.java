fcedb838b8ac924c298f46a1404504d49aef1d1a
SittingDeveloper <rlaaudtlr145@gmail.com>
2022-05-19 16:41:15 +0900

    ex3. Thymeleaf , MVC 시작 th:each 까지 함
    특이사항 :
    #1. 빌드할떄 한번 실패했는데 인텔리제이 재실행하니 정상적으로 작동.
    #2. Edit Configuration 에서 Do Nothing 을 Update로 바꾸니 새로 빌드하지 않아도 변경사항 바로 적용됨 ( 약간 불안정한듯? 노트북 환경이라서 그럴수도 )
    #3 노트북으로 html 주석인 <!-- --> 가 안됌. 주석 표시는 뜨는데 빌드 이후 실행에서 오류 발생

---
+++ ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -0,0 +1,43 @@
+package org.zerock.ex3.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.ex3.dto.SampleDTO;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController {
+    @GetMapping("/ex1")
+    public void ex1() {
+        log.info("ex1.............");
+    }
+
+    // 책과 약간 내용 다르게함.
+    // 람다식 제거 / void exModel -> String exModel 로 변경 후 return 타입으로 html 지정해서 연결,
+    // html 에서 참조하는 변수 찾아갈 수 있게끔 변경
+    @GetMapping("/ex2")
+    public String exModel(Model model) {
+        List<SampleDTO> list = new ArrayList<>();
+
+        for (Long i = 0L; i < 20L; i++) {
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("first.." + i)
+                    .last("last.." + i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            list.add(dto);
+        }
+
+        model.addAttribute("list", list);
+        return "sample/ex2";
+    }
+}
