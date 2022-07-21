97e6824a5df4c124b7e989ef5de6adb41f951b63
SittingDeveloper <rlaaudtlr145@gmail.com>
2022-07-21 10:24:32 +0900

    SpringBoot -> React DTOLIst 전달 완료

--- Task2/src/main/fronted/src/App.js
+++ Task2/src/main/fronted/src/App.js
@@ -2,25 +2,31 @@
 
 import './App.css'
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import axios, {Axios} from 'axios';
 
 function App() {
 
     // 요청받은 정보를 담아줄 변수 선언
-    const [hello, setHello] = useState('')
+    const [list, setList] = useState([])
+
 
     // 첫 번째 렌더링을 마친 후 실행
     useEffect(() => {
-        axios.get('/api/hello')
-            .then(response => setHello(response.data))
+        axios.get('/home')
+            .then(response => setList(response.data))
             .catch(error => console.log(error))
     }, []);
+
+    console.log(list)
 
     return (
         <>
             <header className="App-header">
                 <h1>Show Member</h1>
-                백엔드에서 값을 가져옵니다 : {hello}
+                {list.map((item) =>
+                    <p>Member_id : {item.id} <br/>
+                    String_Data : {item.str_Data} <br/>
+                    Int_Data : {item.int_Data}</p>)}
             </header>
         </>
     );
