70e6c178d5be2849ca44cdbef2839381b7514acd
SittingDeveloper <rlaaudtlr145@gmail.com>
2022-08-17 16:57:38 +0900

    CORS 문제 해결
    #1. docker-compose 파일의 springboot 컨테이너 명으로 setupProxy target을 변경해주어야함

--- task11/src/main/frontend/src/setupProxy.js
+++ task11/src/main/frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
     app.use(
         createProxyMiddleware("/api", {
-            target: "http://localhost:8080", // 비즈니스 서버 URL 설정
+            target: "http://springboot:8080", // 비즈니스 서버 URL 설정
             changeOrigin: true,
         })
     );
